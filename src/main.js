3adfd2068ea7c2406eeca646a87511b441eb1093
Vishal Gupta <69633935+vishalgupta44@users.noreply.github.com>
2020-08-14 23:14:42 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
 $(document).ready(function(){
     $('.header').height($(window).height()/8);
-
+    
     if ($(window).width() <= "755"){
         $('.author-follow').before('<br><div class="follow">+ Follow</div>');
         $('.author-follow').css('margin-left', '30px');
         $('.author-follow').hide();
         $('.vl').hide();
-        $('span').html('<hr>')
+        $('span').html('<hr>');
     }
+    
+    if ($('.about').height() > "400"){
+        $('.vl').height($('.about').height() + 150);
+    }
+
     $('.follow').bind('click', function(){
         $('.author-follow').slideToggle();
-    })
+    });
 });
